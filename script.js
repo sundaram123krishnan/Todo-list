701af1e485cea9b782699182bf72aaf8997d8ac7
sundaram123krishnan <krishnsundaram@gmail.com>
2023-02-05 22:31:04 +0530

    added pending tasks feature

--- script.js
+++ script.js
@@ -2,28 +2,27 @@ let add = document.getElementById("btn");
 let main = document.getElementById("main");
 let list = document.getElementById("list");
 let inp = document.getElementById("inp");
-
+let clear = document.getElementById("clear");
+let arr = [];
+let cnt = 0;
+let cnt1 = 0;
 add.onclick = () => {
   if (inp.value != "") {
     let box = document.createElement("div");
+    box.id = "content";
     let content = document.createElement("div");
     let check = document.createElement("input");
     let del = document.createElement("img");
     del.src = "./delete.jpeg";
-    box.appendChild(check);
     box.appendChild(content);
     box.appendChild(del);
     box.style.display = "flex";
     box.style.gap = "3";
     box.style.justifyContent = "space-between";
     box.firstElementChild.type = "checkbox";
-    check.style.height = "64px";
-    check.style.width = "64px";
-    check.style.cursor = "pointer";
     content.style.backgroundColor = "grey";
     content.innerHTML = inp.value;
     content.style.width = "100%";
-    content.style.marginLeft = "1.5rem";
     content.style.marginRight = "1.5rem";
     content.style.display = "flex";
     content.style.alignItems = "center";
@@ -31,17 +30,28 @@ add.onclick = () => {
     content.style.padding = "0.5rem";
     del.style.height = "64px";
     del.style.cursor = "pointer";
-    del.onmouseover = ()=>{
-        del.style.scale = "1.2";
-    }
-    del.onmouseleave = ()=>{
-        del.style.scale = "1";
-    }
+
+    del.onmouseover = () => {
+      del.style.scale = "1.2";
+    };
+    del.onmouseleave = () => {
+      del.style.scale = "1";
+    };
     main.appendChild(box);
-    del.onclick = ()=>{
-        main.removeChild(box);
-    }
+    cnt++;
+    let ok = true;
+    del.onclick = () => {
+      main.removeChild(box);
+      cnt1++;
+      document.getElementsByTagName("h2")[0].textContent = `U have ${
+        cnt - cnt1
+      } tasks pending`;
+    };
+    document.getElementsByTagName(
+      "h2"
+    )[0].textContent = `U have ${cnt} tasks pending`;
+    arr.push(check);
   }
   inp.value = "";
 };
-
+inp.value="";
